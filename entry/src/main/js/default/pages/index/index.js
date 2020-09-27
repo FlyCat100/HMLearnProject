16d374e249ec1faec41856c06efc1ab61507f5ce
hsf0702 <2573756988@qq.com>
2020-09-27 13:59:57 +0800

    主页携带数据跳转到训练界面

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,11 +1,26 @@
 import router from '@system.router'
+
+var picker1value = null;
+var picker2value = null;
+
 export default {
     data: {
-        title: 'World'
+        picker1range: ["1", "2", "3"],
+        picker2range: ["较慢", "较缓", "较快"]
     },
-    clickAction(){
+    clickAction() {
         router.replace({
-            uri:'pages/xunlian/xunlian'
+            uri: 'pages/xunlian/xunlian',
+            params: {
+                "data1": picker1value,
+                "data2": picker2value
+            }
         })
+    },
+    picker1change(pv) {
+        picker1value = pv.newValue;
+    },
+    picker2change(pv) {
+        picker2value = pv.newValue;
     }
 }
--- entry/src/main/js/default/pages/xunlian/xunlian.js
+++ entry/src/main/js/default/pages/xunlian/xunlian.js
@@ -1,11 +1,30 @@
 import router from '@system.router'
+
+var picker1value = null;
+var picker2value = null;
+var pickerSeconds = null;
+
 export default {
     data: {
-
+        seconds: 0
     },
-    clickAction(){
+    clickAction() {
         router.replace({
-            uri:'pages/index/index'
+            uri: 'pages/index/index'
         })
+    },
+    onInit() {
+        picker1value = this.data1;
+        picker2value = this.data2;
+
+        if (picker1value == "1") {
+            pickerSeconds = 60;
+        } else if (picker1value == "2") {
+            pickerSeconds = 120;
+        } else if (picker1value == "3") {
+            pickerSeconds = 180;
+        }
+
+        this.seconds = pickerSeconds;
     }
 }
